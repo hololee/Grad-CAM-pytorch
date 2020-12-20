307f5502403dfd844998eef47a7671a2b0d71c6f
hololee <lccandol@naver.com>
2020-12-20 16:34:14 +0900

    update readme.md

--- module.py
+++ module.py
@@ -2,7 +2,5 @@ import torch.nn as nn
 
 
 class gradcam(nn.Module):
-    def __init__(self, model, activation_layers = ):
+    def __init__(self, model, activation_layers):
         super().__init__()
-
-        return
