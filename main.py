ae404b7a0e4c217fa063f1ed6aa6dbd50ad2f1de
hololee <lccandol@naver.com>
2020-12-21 18:16:24 +0900

    get Grad-cam, but should check again.

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ import torch
 import torchvision
 import os
 from module import gradcam
+from imageio import imread
+import numpy as np
+import torchvision.transforms.functional as F
+import matplotlib.pyplot as plt
+import cv2
+
+
+def Normalize(img, inplace=False):
+    changed_dim = np.transpose(img, (2, 0, 1)) / 255.
+    Tensor = torch.from_numpy(changed_dim)
+
+    means = [0.485, 0.456, 0.406]
+    stds = [0.229, 0.224, 0.225]
+
+    normalized = F.normalize(Tensor, means, stds, inplace)
+
+    return normalized.unsqueeze(0)
+
 
 if __name__ == '__main__':
     # download resnet weight to ./models
@@ -19,4 +37,31 @@ if __name__ == '__main__':
     # print(list(resnet.layer4._modules))  # '2'
     # last layer of resnet is Y
 
-    grad_cam.forward()
+    origin = imread('/data_ssd3/LJH/pytorch_project/Grad-CAM-pytorch/data/cat.jpg')
+    # normalize for pre-trained network.
+    img = Normalize(origin.astype(np.float32))
+
+    # for calculate gradient.
+    img.requires_grad_(True)
+
+    # forward.
+    grad_cam.forward(img)
+
+    # backward.
+    grad_cam.backward()
+
+    # calculate dy/dA
+    with torch.no_grad():
+        alpha_c_k = np.mean(grad_cam.get_activation_gradient()[0].numpy(), axis=(2, 3))
+        A_k = grad_cam.get_activation()[0].detach().numpy()
+
+        alpha_c_k = alpha_c_k.squeeze()
+        A_k = A_k.squeeze()
+
+        L_Grad_CAM = torch.nn.ReLU()(torch.from_numpy(cv2.resize(np.mean(np.multiply(A_k.transpose(1, 2, 0), alpha_c_k), axis=2), (224, 224), interpolation=cv2.INTER_LANCZOS4)))
+
+        plt.subplot(121)
+        plt.imshow(origin)
+        plt.subplot(122)
+        plt.imshow(L_Grad_CAM, cmap='jet')
+        plt.show()
--- module.py
+++ module.py
@@ -1,17 +1,30 @@
 import torch.nn as nn
+import torch
 
 
 class gradcam(nn.Module):
     def __init__(self, model, activation_module, activation_sub_module):
         super().__init__()
+        # for saved gradient
         self.list_gradient = []
+
+        # for get activation.
         self.activation_a = []
+
         self.model = model
         self.activation_module = activation_module
         self.activation_sub_module = activation_sub_module
 
+        self.output = None
+
     def func_grad(self, gradient):
         self.list_gradient.append(gradient)
+
+    def get_activation_gradient(self):
+        return self.list_gradient
+
+    def get_activation(self):
+        return self.activation_a
 
     def forward(self, x):
 
@@ -21,11 +34,21 @@ class gradcam(nn.Module):
             # apply module.
             x = module(x)
 
+            if index_module == 'avgpool':
+                x = x.view(x.shape[0], -1)
+
             # find activations.
             if index_module == self.activation_module:
                 for index_sub_module, sub_module in module._modules.items():
 
                     if index_sub_module == self.activation_sub_module:
+                        # find gradient of last Conv activation and regist grad hook for get  gradient of activation.
                         x.register_hook(self.func_grad)
                         self.activation_a.append(x)
-        return x
+
+        self.output = x
+
+    def backward(self):
+        # TODO: set to 0 except argmax(output), to make gradcam for specific class
+        output = torch.sum(self.output)
+        output.backward(retain_graph=True)
